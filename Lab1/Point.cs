b81c8677bdb6f98c1664a6a8684f781724d5cb33
Андрей Боев <a.boev.it@gmail.com>
2024-10-26 13:56:26 +0300

    Lab1

---
+++ Lab1/Point.cs
@@ -0,0 +1,40 @@
+﻿namespace Lab1;
+
+public class Point
+{
+    private int x;
+    private int y;
+    private int z;
+
+    public Point(int x, int y, int z)
+    {
+        this.x = x;
+        this.y = y;
+        this.z = z;
+    }
+
+    public Point() : this(0, 0, 0)
+    {
+    }
+
+    public Point(Point point) : this(point.x, point.y, point.z)
+    {
+    }
+
+    public void MoveBy(int dx, int dy, int dz)
+    {
+        x += dx;
+        y += dy;
+        z += dz;
+    }
+
+    public Point Clone()
+    {
+        return new Point(x, y, z);
+    }
+
+    public override string ToString()
+    {
+        return $"({x}, {y}, {z})";
+    }
+}
---
+++ Lab1/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace Lab1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int x, y, z;
+            x = int.Parse(Console.ReadLine());
+            y = int.Parse(Console.ReadLine());
+            z = int.Parse(Console.ReadLine());
+            Point p1 = new Point(x, y, z);
+            Random rnd = new Random();
+            Point p2 = new Point(rnd.Next(-100, 100), rnd.Next(-100, 100), rnd.Next(-100, 100));
+            Point p3 = new Point(p1);
+            Point p4 = p2.Clone();
+
+            Console.WriteLine(p1);
+            Console.WriteLine(p2);
+            Console.WriteLine(p3);
+            Console.WriteLine(p4);
+
+            List<Point> points = new List<Point>();
+            for (int i = 0; i < 5; i++)
+                points.Add(new Point(rnd.Next(-100, 100), rnd.Next(-100, 100), rnd.Next(-100, 100)));
+
+            foreach (var point in points)
+                Console.WriteLine(point);
+
+            points.RemoveAt(1);
+            points.RemoveAt(3);
+
+            foreach (var point in points)
+                Console.WriteLine(point);
+        }
+    }
+}
